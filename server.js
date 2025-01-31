f8bfde3725e87141db36c60bee236cd47696f41d
MarcusLO00 <marcus.lisolivebring@iths.se>
2025-01-31 09:33:26 +0100

    Express api, done

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app =express();
 const PORT = process.env.PORT || 3000;
@@ -14,4 +15,18 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
+});
+
+//byt ut mot .net api URL
+const DOTNET_API_URL = "url här";
+
+//Get all snus from API
+app.get("/snus", async (req, res) => {
+    try {
+        const response =await axios.get(DOTNET_API_URL);
+        res.join(response.data);
+    }
+    catch (error) {
+        console.error(error.message);
+    }
 });
