2c224dde7ff57ed2e5720e5d4bb13cf2533f55cc
MarcusLO00 <marcus.lisolivebring@iths.se>
2025-02-01 11:59:22 +0100

    Server.js done

--- server.js
+++ server.js
@@ -17,16 +17,72 @@ app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 });
 
-//byt ut mot .net api URL
-const DOTNET_API_URL = "url här";
+//.net api URL
+const DOTNET_API_URL = "https://mlsnusapi.azurewebsites.net/snus";
 
 //Get all snus from API
 app.get("/snus", async (req, res) => {
     try {
         const response =await axios.get(DOTNET_API_URL);
-        res.join(response.data);
+        res.json(response.data);
     }
     catch (error) {
         console.error(error.message);
+    }
+});
+
+//Get a snus by id
+app.get("/snus/:id", async (req, res) => {
+    try {
+        const response = await axios.get(`${DOTNET_API_URL}/${req.params.id}`);
+        res.json(response.data);
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).json({error: "server error"})
+    }
+});
+
+//Create new snus 
+app.post ("/snus", async (req, res) => {
+    try {
+        const response = await axios.post(DOTNET_API_URL, req.body, {
+            headers: {"Content-Type": "application/json"}
+        });
+        res.status(201).json(response.data);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({error: "Server error"})
+    }
+});
+
+//Update snus 
+app.put("/snus/:id", async (req, res) => {
+    try {
+        const response = await axios.put(`${DOTNET_API_URL}/${req.params.id}`, req.body, {
+            headers: { "Content-Type": "application/json" }
+        });
+        res.json({ message: "Snus updated" });
+
+    } catch (error) {
+        if(error.response && error.response.status === 404){
+            res.status(404).json({error: "snus not found"});
+        }
+        console.error("Error updating snus", error.message);
+        res.status(500).json ({error: "server error"})
+    }
+});
+
+// Deleta snus 
+app.delete("/snus/:id", async (req, res) => {
+    try {
+        await axios.delete(`${DOTNET_API_URL}/${req.params.id}`);
+        res.json({message: "Snus deleted"});
+
+    } catch (error) {
+        if (error.response && error.response.status === 404) {
+            return res.status(404).json ({error: "Snus not found"});
+        }
+        console.error("Error deleting snus:", error.message);
+        res.status(500).json({ error: "Server error" });
     }
 });
